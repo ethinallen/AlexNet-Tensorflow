e13d322b090b81c6071b98808a21a2690de53561
Ali Gholami <aligholami7596@gmail.com>
2018-03-20 00:47:44 +0330

    avoids redundant coding

--- src/alexnet.py
+++ src/alexnet.py
@@ -19,55 +19,19 @@ FC_HIDDEN_SIZE = 4096
 
 # Global dataset dictionary
 dataset_dict = {
-    "image_size": 256,
+    "image_size": 224,
     "num_channels": 3,
     "num_labels": 22000,
 }
 
 dataset_dict["total_image_size"] = dataset_dict["image_size"] * dataset_dict["image_size"]
 
-# AlexNet Network Definition
-def alex_net(batch_size, patch_size, depth, hidden_size, data):
+def create_conv_layer(data, num_channels, depth, filter_shape, pool_shape, name):
+    '''
+        Generates a new convolutional layer with the given parameters.
+    '''
 
-    # We'll be using this variables frequently
-    image_size = data["image_size"]
-    num_labels = data["num_labels"]
-    num_channels = data["num_channels"]
+    # Define the filter window shape
+    filter_window_shape = [filter_shape[0], filter_shape[1], num_channels, depth]
 
-    # Create the tensorflow graph
-    graph = tf.Graph()
-
-    with graph.as_default():
-        '''
-            as_default() is the method that creates a context manager object corresponding to
-            what you got out of "tf.Graph" That context manager pushes new Graph instance to
-            thread-local stack so that all new TensorFlow ops are created for that graph.
-        '''
-
-        # Define the input placeholder
-        net_input = tf.placeholder(tf.float32, shape = (batch_size, image_size, image_size, num_channels))
-
-        # Define the output placeholder
-        net_output = tf.placeholder(tf.float32, shape = (batch_size, num_labels))
-
-        # Define different layers weights and biases
-        conv1_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, depth]), stddev = 0.1)
-        conv1_biases = tf.Variable(tf.Zeros([depth]))
-
-        conv2_weights = 
-        conv2_biases =
-
-        conv3_weights =
-        conv3_biases =
-
-        conv4_weights =
-        conv4_biases =
-
-        fc1_weights =
-        fc1_biases =
-
-        fc2_weights =
-        fc2_biases =
-
-        fc3_weights =
-        fc3_biases =
+    
