8a22086c57d1a3ce066da4a0a4eaaa00b46f3354
Ali Gholami <aligholami7596@gmail.com>
2018-03-20 14:38:58 +0330

    Local response normalization parameteres were added

--- src/alexnet.py
+++ src/alexnet.py
@@ -20,6 +20,10 @@ MOMENTUM = 0.9
 WEIGHT_DECAY = 0.0005
 DROPOUT_KEEP_PROB = 0.5
 FC_HIDDEN_SIZE = 4096
+K_BIAS = 2
+N_DEPTH_RADIUS = 5
+ALPHA = 1e-4
+BETA = 0.75
 
 # Global dataset dictionary
 dataset_dict = {
@@ -81,14 +85,14 @@ labels = tf.placeholder(tf.float32, shape=[None, dataset_dict["num_labels"]])
 c_layer_1 = tf.nn.conv2d(img_4d_shaped, conv_weights["c1_weights"], strides=[1, 4, 4, 1], padding="SAME", name="c_layer_1")
 c_layer_1 += conv_biases["c1_biases"]
 c_layer_1 = tf.nn.relu(c_layer_1)
-c_layer_1 = tf.nn.lrn(c_layer_1, depth_radius=4.5, bias=1.8, alpha=1e-4, beta=0.75)
+c_layer_1 = tf.nn.lrn(c_layer_1, depth_radius=N_DEPTH_RADIUS, bias=K_BIAS, alpha=ALPHA, beta=BETA)
 c_layer_1 = tf.nn.max_pool(c_layer_1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
 
 # Convolution Layer 2 | Response Normalization | Max Pooling | ReLU
 c_layer_2 = tf.nn.conv2d(c_layer_1, conv_weights["c2_weights"], strides=[1, 1, 1, 1], padding="SAME", name="c_layer_2")
 c_layer_2 += conv_biases["c2_biases"]
 c_layer_2 = tf.nn.relu(c_layer_2)
-c_layer_2 = tf.nn.lrn(c_layer_2, depth_radius=4.5, bias=1.8, alpha=1e-4, beta=0.75)
+c_layer_2 = tf.nn.lrn(c_layer_2, depth_radius=5, bias=K_BIAS, alpha=ALPHA, beta=BETA)
 c_layer_2 = tf.nn.max_pool(c_layer_2, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], padding="VALID")
 
 # Convolution Layer 3 | ReLU
@@ -114,43 +118,3 @@ c_layer_5 = tf.nn.max_pool(c_layer_5, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], 
 
 
 # Fully Connected Layer 3 | Softmax
-
-
-def create_conv_layer(input_shape, num_channels, num_filters, filter_shape, pool_shape, name):
-    '''
-        Generates a new convolutional layer with the given parameters.
-    '''
-
-    # Define the filter window shape
-    filter_window_shape = [filter_shape[0], filter_shape[1], num_channels, num_filters]
-
-    # Set initial weights and biases for the filter
-    filter_weights = tf.Variable(tf.truncated_normal(filter_window_shape), name=name + '_W')
-    filter_biases = tf.Variable(tf.truncated_normal(num_filters), name=name + '_b')
-
-    # Create the main convoloutional layer
-    conv_layer = tf.nn.conv2d(input_shape, filter=filter_weights, strides=[1, 1, 1, 1], padding='SAME', name='CONV_1')
-
-    # Add biases to the filter weights
-    conv_layer += filter_biases
-
-    # ReLU non-linear activation
-    conv_layer = tf.nn.relu(conv_layer, name='ReLU_1')
-
-    # Max pooling layer
-    ksize = [1, pool_shape[0], pool_shape[1], 1]       # Pooling window size | first & last args are always set to 1
-    strides = [1, 2, 2, 1]
-    conv_layer = tf.nn.max_pool(value=input_shape, ksize=ksize, strides=strides, padding='SAME', name='MAX_POOL_1')
-
-    # Return the sub-graph
-    return conv_layer
-
-
-# Generate 5 convolutional layers
-c_layer_1 = create_conv_layer(train_4d_shaped, 3, 32, dataset_dict["filter_shape"], dataset_dict["pool_shape"], name='c_layer_1')
-c_layer_2 = create_conv_layer(c_layer_1, 32, 64, dataset_dict["filter_shape"], dataset_dict["pool_shape"], name='c_layer_2')
-c_layer_3 = create_conv_layer(c_layer_2, 64, 128, dataset_dict["filter_shape"], dataset_dict["pool_shape"], name='c_layer_3')
-c_layer_4 = create_conv_layer(c_layer_3, 128, 256, dataset_dict["filter_shape"], dataset_dict["pool_shape"], name='c_layer_4')
-c_layer_5 = create_conv_layer(c_layer_4, 256, 512, dataset_dict["filter_shape"], dataset_dict["pool_shape"], name='c_layer_5')
-
-# CONV_LAYER_1 | 
