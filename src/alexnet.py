720d39e43916e87b14a9809d115545cf905f1a39
Ali Gholami <aligholami7596@gmail.com>
2018-03-20 09:59:04 +0330

    max_pooling layer was added

--- src/alexnet.py
+++ src/alexnet.py
@@ -35,7 +35,20 @@ def create_conv_layer(data, num_channels, num_filters, filter_shape, pool_shape,
     filter_window_shape = [filter_shape[0], filter_shape[1], num_channels, num_filters]
 
     # Set initial weights and biases for the filter
-    weights = tf.Variable(tf.truncated_normal(filter_window_shape), stddev=0.1, name=name + '_W')
-    biases = tf.Variable(tf.truncated_normal(num_filters), stddev=0.1, name=name + '_b')
+    filter_weights = tf.Variable(tf.truncated_normal(filter_window_shape), name=name + '_W')
+    filter_biases = tf.Variable(tf.truncated_normal(num_filters), name=name + '_b')
 
-    
+    # Create the main convoloutional layer
+    conv_layer = tf.nn.conv2d(data, filter=filter_weights, strides=[1, 1, 1, 1], padding='SAME', name='CONV_1')
+
+    # Add biases to the filter weights
+    conv_layer += filter_biases
+
+    # ReLU non-linear activation
+    conv_layer = tf.nn.relu(conv_layer, name='ReLU_1')
+
+    # Max pooling layer
+    ksize = [1, pool_shape[0], pool_shape[1], 1]       # Pooling window size | first & last args are always set to 1
+    strides = [1, 2, 2, 1]
+    conv_layer = tf.nn.max_pool(value=data, ksize=ksize, strides=strides, padding='SAME', name='MAX_POOL_1')
+
